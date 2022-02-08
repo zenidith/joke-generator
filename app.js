a4ecd397adadad319dd6144ecdd3029455f5fa61
K Sheenan <kaynesheenan+git@gmail.com>
2022-02-08 23:12:19 +0900

    start JS with basic constants

---
+++ app.js
@@ -0,0 +1,5 @@
+const joke = document.getElementById('joke');
+const get_joke = document.getElementById('get_joke');
+
+// add Eventlistener to the button
+get_joke.addEventListener('click', generateJoke);
