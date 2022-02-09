45a52f4572b653e45ac178ce1fc4ed60188f6fb1
K Sheenan <kaynesheenan+git@gmail.com>
2022-02-09 13:13:15 +0900

    async function

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const get_joke = document.getElementById('get_joke');
 
 // add Eventlistener to the button
 get_joke.addEventListener('click', generateJoke);
+
+async function generateJoke() {
+    // call the joke from API
+    const jokeRes = await fetch('https://icanhazdadjoke.com/',
+    {
+        headers: {
+            accept: 'application/json'
+    })
+    // set Random Joke
+}
