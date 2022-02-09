f3f00eb88eb27d89b336631a3caa6937e975b683
K Sheenan <kaynesheenan+git@gmail.com>
2022-02-09 13:16:40 +0900

    api is working in console

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const get_joke = document.getElementById('get_joke');
 // add Eventlistener to the button
 get_joke.addEventListener('click', generateJoke);
 
+generateJoke();
+
 async function generateJoke() {
     // call the joke from API
     const jokeRes = await fetch('https://icanhazdadjoke.com/',
     {
         headers: {
             accept: 'application/json'
-    })
+        }
+    });
+
+    const joke = await jokeRes.json();
+    console.log(joke);
     // set Random Joke
 }
